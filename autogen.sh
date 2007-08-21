d7aeed2e008c7032d41b8f2815f517580dd957ce
Martin Aumüller <aumuell@reserv.at>
2007-08-21 22:02:35 +0000

    compile virvo & deskvox with one make command
    git-svn-id: https://deskvox.svn.sourceforge.net/svnroot/deskvox/trunk@92 04231f92-3938-0410-9931-e931ac552b4f

---
+++ autogen.sh
@@ -0,0 +1,20 @@
+#! /bin/sh
+aclocal -I virvo/m4 -I vox-desk/m4
+autoheader
+automake --add-missing --copy --foreign
+autoconf
+if [ -f config.status ]; then
+   ./config.status --recheck && ./config.status
+else
+   MACHINE=`uname -m`
+   case $MACHINE in
+      x86_64*) ARCH=amd64 ;;
+      i386*|i486*|i586*|i686*) ARCH=gcc3 ;;
+   esac
+   
+   if [ -z "$*" -a ! -z "$ARCH" ]; then
+       ./configure --enable-cg --with-cg=`pwd` --with-cg-libs=`pwd`/lib/$ARCH
+   else
+       ./configure $*
+   fi
+fi
