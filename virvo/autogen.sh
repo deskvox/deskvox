990fd73a0d8eaa5f8690b58784f6042f0b60a773
Martin Aumüller <aumuell@reserv.at>
2010-08-29 19:10:24 +0000

    exit on error during autotools run
    git-svn-id: https://deskvox.svn.sourceforge.net/svnroot/deskvox/trunk@572 04231f92-3938-0410-9931-e931ac552b4f

--- virvo/autogen.sh
+++ virvo/autogen.sh
@@ -1,9 +1,9 @@
 #! /bin/sh
-aclocal -I m4
-libtoolize || glibtoolize
-autoheader
-automake --add-missing --copy --foreign
-autoconf
+aclocal -I m4 || exit 1
+libtoolize || glibtoolize || exit 1
+autoheader || exit 1
+automake --add-missing --copy --foreign || exit 1
+autoconf || exit 1
 
 if [ -z "$*" -a -f config.status ]; then
    ./config.status --recheck && ./config.status
