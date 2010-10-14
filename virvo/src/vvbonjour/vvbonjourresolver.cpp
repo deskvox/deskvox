8ad94256a4840fe0bc66cfd6ea1c7ebaa8853960
Martin Aumüller <aumuell@reserv.at>
2010-10-14 21:56:11 +0000

    compile fixes
    git-svn-id: https://deskvox.svn.sourceforge.net/svnroot/deskvox/trunk@693 04231f92-3938-0410-9931-e931ac552b4f

--- virvo/src/vvbonjour/vvbonjourresolver.cpp
+++ virvo/src/vvbonjour/vvbonjourresolver.cpp
@@ -19,8 +19,8 @@
 // Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 
 #include "vvbonjourresolver.h"
-#include "vvdebugmsg.h"
-#include "vvsocket.h"
+#include "../vvdebugmsg.h"
+#include "../vvsocket.h"
 
 #include <iostream>
 #include <vector>
