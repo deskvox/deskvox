1380634bd0a328cca8f8095f43291f4531c799f2
Stavros Delisavas <stavros.delisavas@uni-koeln.de>
2011-03-15 11:58:34 +0000

    Fix forgotten rename for headerguard
    git-svn-id: https://deskvox.svn.sourceforge.net/svnroot/deskvox/trunk@1022 04231f92-3938-0410-9931-e931ac552b4f

--- virvo/src/vvibrserver.h
+++ virvo/src/vvibrserver.h
@@ -18,8 +18,8 @@
 // License along with this library (see license.txt); if not, write to the
 // Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 
-#ifndef _VV_ISASERVER_H_
-#define _VV_ISASERVER_H_
+#ifndef _VV_IBRSERVER_H_
+#define _VV_IBRSERVER_H_
 
 #include "vvexport.h"
 #include "vvoffscreenbuffer.h"
