a7f661058748149c42d559f0091f95d7701a70e0
Stefan Zellmann <zellmans@uni-koeln.de>
2011-04-05 10:48:21 +0000

    Create vvversioninfo.h
    Batch script creates vvversioninfo.h. Script call is triggered
    as a pre-build event from vc project and header is placed in
    the virvo/virvo dir. Defines in the header are dummies, this
    commit is just a compile fix.
    
    git-svn-id: https://deskvox.svn.sourceforge.net/svnroot/deskvox/trunk@1053 04231f92-3938-0410-9931-e931ac552b4f

---
+++ virvo/makeversion.bat
@@ -0,0 +1,7 @@
+echo #ifndef VV_VERSION_INFO_H > "%1"\virvo\vvversioninfo.h
+echo #define VV_VERSION_INFO_H >> "%1"\virvo\vvversioninfo.h
+echo #define VV_SVN_REVISION "unknown" >> "%1"\virvo\vvversioninfo.h
+echo #define VV_VERSION "2" >> "%1"\virvo\vvversioninfo.h
+echo #define VV_RELEASE "01b" >> "%1"\virvo\vvversioninfo.h
+echo #define VV_YEAR 2005 >> "%1"\virvo\vvversioninfo.h
+echo #endif >> "%1"\virvo\vvversioninfo.h
