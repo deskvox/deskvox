2ed5d0ae45a27e57ee922cf355ed0bb95f041beb
Martin Aumüller <aumuell@reserv.at>
2011-09-27 06:50:18 +0000

    fix image height
    git-svn-id: https://deskvox.svn.sourceforge.net/svnroot/deskvox/trunk@1312 04231f92-3938-0410-9931-e931ac552b4f

--- virvo/virvo/vvibrserver.cpp
+++ virvo/virvo/vvibrserver.cpp
@@ -81,7 +81,7 @@ void vvIbrServer::renderImage(vvMatrix& pr, vvMatrix& mv, vvRenderer* renderer)
   // Fetch rendered image
   vvGLTools::Viewport vp = vvGLTools::getViewport();
   const int w = vp[2];
-  const int h = vp[2];
+  const int h = vp[3];
   if(!_image || _image->getWidth() != w || _image->getHeight() != h || _image->getDepthPrecision() != dp)
   {
     delete[] _pixels;
