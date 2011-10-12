bb05cb379b0e06e6cf7645e9703d21b2fec4b119
Stavros Delisavas <stavros.delisavas@uni-koeln.de>
2011-10-12 10:15:15 +0000

    fix glew-find-module for windows
    git-svn-id: https://deskvox.svn.sourceforge.net/svnroot/deskvox/trunk@1422 04231f92-3938-0410-9931-e931ac552b4f

--- cmake/FindGLEW.cmake
+++ cmake/FindGLEW.cmake
@@ -9,15 +9,10 @@
 
 IF (WIN32)
 	FIND_PATH( GLEW_INCLUDE_DIR GL/glew.h
-		$ENV{PROGRAMFILES}/GLEW/include
-		${PROJECT_SOURCE_DIR}/src/nvgl/glew/include
 		DOC "The directory where GL/glew.h resides")
 	FIND_LIBRARY( GLEW_LIBRARY
 		NAMES glew GLEW glew32 glew32s
 		PATHS
-		$ENV{PROGRAMFILES}/GLEW/lib
-		${PROJECT_SOURCE_DIR}/src/nvgl/glew/bin
-		${PROJECT_SOURCE_DIR}/src/nvgl/glew/lib
 		DOC "The GLEW library")
 ELSE (WIN32)
 	FIND_PATH( GLEW_INCLUDE_DIR GL/glew.h
@@ -38,11 +33,5 @@ ELSE (WIN32)
 		DOC "The GLEW library")
 ENDIF (WIN32)
 
-IF (GLEW_INCLUDE_DIR)
-	SET( GLEW_FOUND 1 CACHE STRING "Set to 1 if GLEW is found, 0 otherwise")
-ELSE (GLEW_INCLUDE_DIR)
-	SET( GLEW_FOUND 0 CACHE STRING "Set to 1 if GLEW is found, 0 otherwise")
-ENDIF (GLEW_INCLUDE_DIR)
-
-MARK_AS_ADVANCED( GLEW_FOUND )
-
+INCLUDE(FindPackageHandleStandardArgs)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(GLEW DEFAULT_MSG GLEW_LIBRARY GLEW_INCLUDE_DIR)
