137a87a11221030f334cd440e78e11ef2d9ee70a
Stefan Zellmann <zellmans@uni-koeln.de>
2012-03-30 23:16:59 +0000

    Prefer VIRVOEXPORT over __declspec(dllexport)
    git-svn-id: https://deskvox.svn.sourceforge.net/svnroot/deskvox/trunk@1669 04231f92-3938-0410-9931-e931ac552b4f

--- virvo/virvo/vvvecmath.cpp
+++ virvo/virvo/vvvecmath.cpp
@@ -27,8 +27,8 @@
 #include "vvvecmath.h"
 
 #ifdef _WIN32
-template class __declspec(dllexport) vvBaseVector4<float>;
-template class __declspec(dllexport) vvBaseVector3<float>;
+template class VIRVOEXPORT vvBaseVector4<float>;
+template class VIRVOEXPORT vvBaseVector3<float>;
 #endif
 
 #ifdef __sun
