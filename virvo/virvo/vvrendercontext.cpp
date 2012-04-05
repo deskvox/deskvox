c166270333bf08f631cf8d369d47e60d955bf470
Stefan Zellmann <zellmans@uni-koeln.de>
2012-04-05 11:26:08 +0000

    msvc warning fixes
    Microsoft compiler doesn't understand 'and', prefer '&&'
    
    git-svn-id: https://deskvox.svn.sourceforge.net/svnroot/deskvox/trunk@1695 04231f92-3938-0410-9931-e931ac552b4f

--- virvo/virvo/vvrendercontext.cpp
+++ virvo/virvo/vvrendercontext.cpp
@@ -32,7 +32,7 @@ struct ContextArchData
   vvCocoaGLContext* cocoaContext;
 #endif
 
-#if defined(HAVE_X11) and defined(USE_X11)
+#if defined(HAVE_X11) && defined(USE_X11)
   GLXContext glxContext;
   Display* display;
   Drawable drawable;
@@ -59,7 +59,7 @@ vvRenderContext::~vvRenderContext()
     delete _archData->cocoaContext;
 #endif
 
-#if defined(HAVE_X11) and defined(USE_X11)
+#if defined(HAVE_X11) && defined(USE_X11)
     glXDestroyContext(_archData->display, _archData->glxContext);
     switch (_options->type)
     {
@@ -86,7 +86,7 @@ bool vvRenderContext::makeCurrent() const
     return _archData->cocoaContext->makeCurrent();
 #endif
 
-#if defined(HAVE_X11) and defined(USE_X11)
+#if defined(HAVE_X11) && defined(USE_X11)
     return glXMakeCurrent(_archData->display, _archData->drawable, _archData->glxContext);
 #endif
   }
@@ -101,7 +101,7 @@ void vvRenderContext::swapBuffers() const
     _archData->cocoaContext->swapBuffers();
 #endif
 
-#if defined(HAVE_X11) and defined(USE_X11)
+#if defined(HAVE_X11) && defined(USE_X11)
     glXSwapBuffers(_archData->display, _archData->drawable);
 #endif
   }
@@ -119,7 +119,7 @@ void vvRenderContext::resize(const int w, const int h)
       _archData->cocoaContext->resize(w, h);
 #endif
 
-#if defined(HAVE_X11) and defined(USE_X11)
+#if defined(HAVE_X11) && defined(USE_X11)
       switch (_options->type)
       {
       case vvContextOptions::VV_PBUFFER:
@@ -150,7 +150,7 @@ void vvRenderContext::init()
   _initialized = true;
 #endif
 
-#if defined(HAVE_X11) and defined(USE_X11)
+#if defined(HAVE_X11) && defined(USE_X11)
   _archData->display = XOpenDisplay(_options->displayName.c_str());
 
   _archData->attributes.push_back(GLX_RGBA);
@@ -249,7 +249,7 @@ void vvRenderContext::init()
 
 bool vvRenderContext::initPbuffer()
 {
-#if defined(HAVE_X11) and defined(USE_X11)
+#if defined(HAVE_X11) && defined(USE_X11)
   int nelements;
   _archData->fbConfigs = glXChooseFBConfig(_archData->display, DefaultScreen(_archData->display),
                                            &(_archData->attributes)[1], &nelements); // first entry (GLX_RGBA) in attributes list confuses pbuffers
