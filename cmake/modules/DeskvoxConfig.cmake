d52d972acd33023edea3770fd1859830ae2e4bc5
Alexander Bolz <lt.morris.schaffer@googlemail.com>
2012-07-30 15:58:01 +0200

    Add function to replace compiler options

--- cmake/modules/DeskvoxConfig.cmake
+++ cmake/modules/DeskvoxConfig.cmake
@@ -15,6 +15,33 @@ endif()
 
 
 #---------------------------------------------------------------------------------------------------
+# deskvox_replace_compiler_option(var, old, new)
+#
+# Replaces a compiler option or switch <old> in <var> by <new>
+# If <old> is not in <var>, appends <new> to <var>
+# Example:
+#
+#	deskvox_replace_compiler_option(CMAKE_CXX_FLAGS_RELEASE "-O3" "-O2")
+#
+
+
+function(deskvox_replace_compiler_option var old new)
+  # If the option already is on the variable, don't add it:
+  if("${${var}}" MATCHES "(^| )${new}($| )")
+    set(n "")
+  else()
+    set(n "${new}")
+  endif()
+  if("${${var}}" MATCHES "(^| )${old}($| )")
+    string(REGEX REPLACE "(^| )${old}($| )" " ${n} " ${var} "${${var}}")
+  else()
+    set(${var} "${${var}} ${n}")
+  endif()
+  set(${var} "${${var}}" PARENT_SCOPE)
+endfunction()
+
+
+#---------------------------------------------------------------------------------------------------
 # include checks
 #
 
