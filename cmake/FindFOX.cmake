2a1343be26d9177f604ecd565605b546c55d2e17
Alexander Bolz <lt.morris.schaffer@googlemail.com>
2012-07-31 15:40:26 +0200

    Fix/clean-up find modules

--- cmake/FindFOX.cmake
+++ cmake/FindFOX.cmake
@@ -1,22 +1,58 @@
 include(FindPackageHandleStandardArgs)
 
+set(hints
+  $ENV{LIB_BASE_PATH}/fox
+)
+
+set(paths
+  /usr
+  /usr/local
+)
+
 find_path(FOX_INCLUDE_DIR
-  NAMES fx.h
+  NAMES
+    fx.h
   HINTS
-    /usr/include
-    /usr/include/fox-1.6
-    /usr/local/include
-    /usr/local/include/fox-1.6
+    ${hints}
+  PATHS
+    ${paths}
+  PATH_SUFFIXES
+    include
+    include/fox
+    include/fox-1.6
 )
 
 find_library(FOX_LIBRARY
-  NAMES FOX-1.6 fox-1.6
+  NAMES
+    FOX-1.6
+    fox-1.6
+  HINTS
+    ${hints}
   PATHS
-    /usr/lib64
-    /usr/lib
-    /usr/local/lib64
-    /usr/local/lib
+    ${paths}
+  PATH_SUFFIXES
+    lib64
+    lib
 )
+
+find_library(FOX_LIBRARY_DEBUG
+  NAMES
+    FOXD-1.6
+    foxd-1.6
+  HINTS
+    ${hints}
+  PATHS
+    ${paths}
+  PATH_SUFFIXES
+    lib64
+    lib
+)
+
+if(FOX_LIBRARY_DEBUG)
+  set(FOX_LIBRARIES optimized ${FOX_LIBRARY} debug ${FOX_LIBRARY_DEBUG})
+else()
+  set(FOX_LIBRARIES ${FOX_LIBRARY})
+endif()
 
 find_package_handle_standard_args(FOX DEFAULT_MSG
   FOX_INCLUDE_DIR
--- cmake/FindPthreads.cmake
+++ cmake/FindPthreads.cmake
@@ -1,41 +1,72 @@
 include(FindPackageHandleStandardArgs)
 
-if(NOT DEFINED Pthreads_EXCEPTION_MODE)
-  set(Pthreads_EXCEPTION_MODE "C")
+set(hints
+  $ENV{LIB_BASE_PATH}/pthread
+  $ENV{LIB_BASE_PATH}/pthreads
+)
+
+set(paths
+  /usr
+  /usr/local
+)
+
+find_path(Pthreads_INCLUDE_DIR
+  NAMES
+    pthread.h
+  HINTS
+    ${hints}
+  PATHS
+    ${paths}
+  PATH_SUFFIXES
+    include
+    include/pthread
+    include/pthreads
+)
+
+if(NOT Pthreads_EXCEPTION_SCHEME)
+  set(Pthreads_EXCEPTION_SCHEME "C")
 else()
-  if(NOT Pthreads_EXCEPTION_MODE STREQUAL "C" AND
-     NOT Pthreads_EXCEPTION_MODE STREQUAL "CE" AND
-     NOT Pthreads_EXCEPTION_MODE STREQUAL "SE")
+  if(NOT Pthreads_EXCEPTION_SCHEME STREQUAL "C" AND
+     NOT Pthreads_EXCEPTION_SCHEME STREQUAL "CE" AND
+     NOT Pthreads_EXCEPTION_SCHEME STREQUAL "SE")
     message(FATAL_ERROR "Only C, CE, and SE exception modes are allowed")
   endif()
-  if(NOT MSVC AND Pthreads_EXCEPTION_MODE STREQUAL "SE")
+  if(NOT MSVC AND Pthreads_EXCEPTION_SCHEME STREQUAL "SE")
     message(FATAL_ERROR "Structured Exception Handling is only allowed for MSVC")
   endif()
 endif()
 
-find_path(Pthreads_INCLUDE_DIR
-  NAMES pthread.h
-  HINTS
-    /usr/include
-    /usr/local/include
-)
-
-if(MSVC)
-  set(libnames pthreadV${Pthreads_EXCEPTION_SCHEME}2 pthread)
-else()
-  set(libnames pthread)
-endif()
-
 find_library(Pthreads_LIBRARY
-  NAMES ${libnames}
+  NAMES
+    pthreadV${Pthreads_EXCEPTION_SCHEME}2
+    pthread
+  HINTS
+    ${hints}
   PATHS
-    /usr/lib64
-    /usr/lib
-    /usr/local/lib64
-    /usr/local/lib
-    /lib64
-    /lib
+    ${paths}
+  PATH_SUFFIXES
+    lib64
+    lib
 )
+
+find_library(Pthreads_LIBRARY_DEBUG
+  NAMES
+    pthreadV${Pthreads_EXCEPTION_SCHEME}2d
+    pthreadd
+  HINTS
+    ${hints}
+  PATHS
+    ${paths}
+  PATH_SUFFIXES
+    lib64
+    lib
+)
+
+if(Pthreads_LIBRARY_DEBUG)
+  set(Pthreads_LIBRARIES optimized ${Pthreads_LIBRARY} debug ${Pthreads_LIBRARY_DEBUG})
+else()
+  set(Pthreads_LIBRARIES ${Pthreads_LIBRARY})
+endif()
 
 find_package_handle_standard_args(Pthreads DEFAULT_MSG
   Pthreads_INCLUDE_DIR
