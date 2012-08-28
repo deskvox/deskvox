4e451a5b83acf9675b5d60320069d804a9b93141
Stefan Zellmann <zellmans@uni-koeln.de>
2012-08-28 11:31:10 +0200

    Fix typo in header guard

--- vox-desk-qt/plugins/servermon/vvservermondialog.h
+++ vox-desk-qt/plugins/servermon/vvservermondialog.h
@@ -19,7 +19,7 @@
 // Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 
 #ifndef VV_SERVERMONDIALOG_H
-#define VV_SERVERMONDIALOG_h
+#define VV_SERVERMONDIALOG_H
 
 #include <QDialog>
 
