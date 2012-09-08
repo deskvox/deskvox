57c3189044727083969d0af445e943689469d2dc
Stefan Zellmann <zellmans@uni-koeln.de>
2012-09-08 10:15:40 +0200

    Forgot to add file to version control

---
+++ vox-desk-qt/src/vvprefdialog.cpp
@@ -0,0 +1,66 @@
+// Virvo - Virtual Reality Volume Rendering
+// Copyright (C) 1999-2003 University of Stuttgart, 2004-2005 Brown University
+// Contact: Jurgen P. Schulze, jschulze@ucsd.edu
+//
+// This file is part of Virvo.
+//
+// Virvo is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library (see license.txt); if not, write to the
+// Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+
+#include "vvprefdialog.h"
+
+#include "ui_vvprefdialog.h"
+
+#include <virvo/vvdebugmsg.h>
+
+#include <cassert>
+#include <iostream>
+#include <limits>
+
+vvPrefDialog::vvPrefDialog(QWidget* parent)
+  : QDialog(parent)
+  , ui(new Ui_PrefDialog)
+{
+  vvDebugMsg::msg(1, "vvPrefDialog::vvPrefDialog()");
+
+  ui->setupUi(this);
+}
+
+void vvPrefDialog::toggleInterpolation()
+{
+  vvDebugMsg::msg(3, "vvPrefDialog::toggletInterpolation()");
+
+  const bool interpolation = ui->interpolationCheckBox->isChecked();
+  ui->interpolationCheckBox->setChecked(!interpolation);
+  emit parameterChanged(vvRenderer::VV_SLICEINT, !interpolation);
+}
+
+void vvPrefDialog::scaleStillQuality(const float s)
+{
+  vvDebugMsg::msg(3, "vvPrefDialog::scaleStillQuality()");
+
+  assert(s >= 0.0f);
+
+  float quality = static_cast<float>(ui->stillSpinBox->value());
+  if (quality <= 0.0f)
+  {
+    // never let quality drop to or below 0
+    quality = std::numeric_limits<float>::epsilon();
+  }
+  quality *= s;
+  
+  ui->stillSpinBox->setValue(quality);
+  emit parameterChanged(vvRenderer::VV_QUALITY, quality);
+}
+
