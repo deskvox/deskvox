1845e2f882c07d04164e248910143a28d2727679
Stefan Zellmann <info@szellmann.de>
2012-09-15 11:43:56 +0200

    Fix header guard
    VV_COCOAGLCONTEST_H ==> VV_COCOAGLCONTEXT_H

--- virvo/virvo/vvcocoaglcontext.h
+++ virvo/virvo/vvcocoaglcontext.h
@@ -19,7 +19,7 @@
 // Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 
 #ifndef VV_COCOAGLCONTEXT_H
-#define VV_COCOAGLCONTEST_H
+#define VV_COCOAGLCONTEXT_H
 
 #ifdef __APPLE__
 
