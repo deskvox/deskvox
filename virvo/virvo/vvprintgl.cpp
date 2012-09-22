293957c86cd81b18ddcec1eeb2dd7ef0ac792754
Stefan Zellmann <info@szellmann.de>
2012-09-22 15:42:58 +0200

    vvprintgl needs config header

--- virvo/virvo/vvprintgl.cpp
+++ virvo/virvo/vvprintgl.cpp
@@ -18,6 +18,10 @@
 // License along with this library (see license.txt); if not, write to the
 // Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 
+#ifdef HAVE_CONFIG_H
+#include "vvconfig.h"
+#endif
+
 #include "vvplatform.h"
 #include <string.h>
 #include <stdarg.h>
