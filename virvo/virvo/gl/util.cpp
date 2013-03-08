2e064f894a8d1bee0f4836178488fbdfa87af64d
abolz <lt.morris.schaffer@gmail.com>
2013-03-08 13:53:04 +0100

    Compile with GLEW < 1.9.0

--- virvo/virvo/gl/util.cpp
+++ virvo/virvo/gl/util.cpp
@@ -122,7 +122,7 @@ static void APIENTRY DebugCallback( GLenum /*source*/,
 //--------------------------------------------------------------------------------------------------
 void gl::enableDebugCallback()
 {
-#ifndef NDEBUG
+#if !defined(NDEBUG) && defined(GL_KHR_debug)
     if (GLEW_KHR_debug)
     {
         glEnable(GL_DEBUG_OUTPUT);
@@ -157,58 +157,22 @@ static char const* GetFramebufferStatusString(GLenum status)
     {
     case GL_FRAMEBUFFER_COMPLETE:
         return "framebuffer complete";
-
     case GL_FRAMEBUFFER_UNDEFINED:
-        // is returned if target is the default framebuffer, but the default
-        // framebuffer does not exist.
         return "framebuffer undefined";
-
     case GL_FRAMEBUFFER_INCOMPLETE_ATTACHMENT:
-        // is returned if any of the framebuffer attachment points are
-        // framebuffer incomplete.
         return "framebuffer incomplete attachment";
-
     case GL_FRAMEBUFFER_INCOMPLETE_MISSING_ATTACHMENT:
-        // is returned if the framebuffer does not have at least one image
-        // attached to it.
         return "framebuffer incomplete missing attachment";
-
     case GL_FRAMEBUFFER_INCOMPLETE_DRAW_BUFFER:
-        // is returned if the value of GL_FRAMEBUFFER_ATTACHMENT_OBJECT_TYPE
-        // is GL_NONE for any color attachment point(s) named by GL_DRAWBUFFERi.
         return "framebuffer incomplete draw buffer";
-
     case GL_FRAMEBUFFER_INCOMPLETE_READ_BUFFER:
-        // is returned if GL_READ_BUFFER is not GL_NONE and the value of
-        // GL_FRAMEBUFFER_ATTACHMENT_OBJECT_TYPE is GL_NONE for the color
-        // attachment point named by GL_READ_BUFFER.
         return "framebuffer incomplete read buffer";
-
     case GL_FRAMEBUFFER_UNSUPPORTED:
-        // is returned if the combination of internal formats of the attached
-        // images violates an implementation-dependent set of restrictions.
         return "framebuffer unsupported";
-
     case GL_FRAMEBUFFER_INCOMPLETE_MULTISAMPLE:
-        // is returned if the value of GL_RENDERBUFFER_SAMPLES is not the same
-        // for all attached renderbuffers; if the value of GL_TEXTURE_SAMPLES is
-        // the not same for all attached textures; or, if the attached images
-        // are a mix of renderbuffers and textures, the value of
-        // GL_RENDERBUFFER_SAMPLES does not match the value of
-        // GL_TEXTURE_SAMPLES.
-        // GL_FRAMEBUFFER_INCOMPLETE_MULTISAMPLE  is also returned if the value
-        // of GL_TEXTURE_FIXED_SAMPLE_LOCATIONS is not the same for all attached
-        // textures; or, if the attached images are a mix of renderbuffers and
-        // textures, the value of GL_TEXTURE_FIXED_SAMPLE_LOCATIONS is not
-        // GL_TRUE for all attached textures.
         return "framebuffer incomplete multisample";
-
     case GL_FRAMEBUFFER_INCOMPLETE_LAYER_TARGETS:
-        // is returned if any framebuffer attachment is layered, and any
-        // populated attachment is not layered, or if all populated color
-        // attachments are not from textures of the same target.
         return "framebuffer incomplete layer targets";
-
     default:
         return "{unknown status}";
     }
