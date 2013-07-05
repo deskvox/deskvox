89212a778f024780a772850326cbe315366983ca
abolz <lt.morris.schaffer@gmail.com>
2013-07-05 11:39:10 +0200

    Export createRayRend

--- virvo/virvo/vvrayrendfactory.h
+++ virvo/virvo/vvrayrendfactory.h
@@ -1,6 +1,8 @@
 #pragma once
 
+#include "vvexport.h"
+
 /*! \brief  factory function, can e. g. be called by dlsym() to create rayrend from within plugin
  */
-extern "C" vvRenderer* createRayRend(vvVolDesc* vd, vvRenderState const& rs);
+extern "C" VVAPI vvRenderer* createRayRend(vvVolDesc* vd, vvRenderState const& rs);
 
