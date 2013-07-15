15429c01b69a206fb17b97291f975aef2b41f0ea
Martin Aumüller <aumuell@reserv.at>
2013-07-15 15:07:37 +0200

    compile fix proposed by Stefan

--- virvo/virvo/vvforceinline.h
+++ virvo/virvo/vvforceinline.h
@@ -5,7 +5,7 @@
 #if VV_CXX_INTEL
 #define VV_FORCE_INLINE __forceinline
 #elif VV_CXX_GCC
-#define VV_FORCE_INLINE __attribute((always_inline))
+#define VV_FORCE_INLINE inline __attribute((always_inline))
 #elif VV_CXX_MSVC
 #define VV_FORCE_INLINE __forceinline
 #else
