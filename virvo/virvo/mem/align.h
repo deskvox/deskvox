f0fbec6646ac638d3aa657dfd3b21fa341d86127
Martin Aumüller <aumuell@reserv.at>
2013-07-15 16:17:20 +0200

    os x compile fix
    ALIGN already defined in /usr/include/i386/param.h

--- virvo/virvo/mem/align.h
+++ virvo/virvo/mem/align.h
@@ -1,11 +1,11 @@
 #pragma once
 
 #if defined(_MSC_VER)
-#define ALIGN(X) __declspec(align(X))
+#define VVALIGN(X) __declspec(align(X))
 #else
-#define ALIGN(X) __attribute__((aligned(X)))
+#define VVALIGN(X) __attribute__((aligned(X)))
 #endif
 
 #define CACHE_LINE 16
-#define CACHE_ALIGN ALIGN(CACHE_LINE)
+#define CACHE_ALIGN VVALIGN(CACHE_LINE)
 
