55778342becf18ef71611f9332db2421e423ea03
Stefan Zellmann <zellmans@uni-koeln.de>
2014-05-29 13:01:10 +0200

    Adhere to 'force inline' when compiing with clang

--- virvo/virvo/vvforceinline.h
+++ virvo/virvo/vvforceinline.h
@@ -4,7 +4,7 @@
 
 #if VV_CXX_INTEL
 #define VV_FORCE_INLINE __forceinline
-#elif VV_CXX_GCC
+#elif VV_CXX_GCC || VV_CXX_CLANG
 #define VV_FORCE_INLINE inline __attribute((always_inline))
 #elif VV_CXX_MSVC
 #define VV_FORCE_INLINE __forceinline
