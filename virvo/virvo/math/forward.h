6f77a3d35815d8a1df8980e121804e273fb19ff5
Stefan Zellmann <zellmans@uni-koeln.de>
2014-05-31 13:25:21 +0200

    Some typedefs

--- virvo/virvo/math/forward.h
+++ virvo/virvo/math/forward.h
@@ -20,6 +20,28 @@ template < size_t Dim, typename T >
 class vector;
 
 
+//--------------------------------------------------------------------------------------------------
+// Most common typedefs
+//
+
+typedef vector< 2, int >                vec2i;
+typedef vector< 2, unsigned int >       vec2ui;
+typedef vector< 2, float >              vec2f;
+typedef vector< 2, double >             vec2d;
+
+
+typedef vector< 3, int >                vec3i;
+typedef vector< 3, unsigned int >       vec3ui;
+typedef vector< 3, float >              vec3f;
+typedef vector< 3, double >             vec3d;
+
+
+typedef vector< 4, int >                vec4i;
+typedef vector< 4, unsigned int >       vec4ui;
+typedef vector< 4, float >              vec4f;
+typedef vector< 4, double >             vec4d;
+
+
 } // math
 
 
