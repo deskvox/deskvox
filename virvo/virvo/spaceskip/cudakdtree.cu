6d87a441594ad6cfc6ebbc1f22f217e27df201b5
Stefan Zellmann <info@szellmann.de>
2018-11-14 00:03:12 +0100

    Test cached_allocator

--- virvo/virvo/spaceskip/cudakdtree.cu
+++ virvo/virvo/spaceskip/cudakdtree.cu
@@ -18,6 +18,14 @@
 // License along with this library (see license.txt); if not, write to the
 // Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 
+#include <thrust/system/cuda/vector.h>
+#include <thrust/system/cuda/execution_policy.h>
+#include <thrust/host_vector.h>
+#include <thrust/generate.h>
+#include <thrust/pair.h>
+#include <iostream>
+#include <map>
+
 #include <cassert>
 #include <iostream>
 #include <ostream>
@@ -56,6 +64,100 @@ using namespace visionaray;
 //-------------------------------------------------------------------------------------------------
 //
 //
+
+class cached_allocator
+{
+public:
+    // just allocate bytes
+    typedef char value_type;
+ 
+    cached_allocator() { }
+ 
+    ~cached_allocator()
+    {
+        // free all allocations when cached_allocator goes out of scope
+        free_all();
+    }
+ 
+    char* allocate(std::ptrdiff_t num_bytes)
+    {
+        char* result = 0;
+ 
+        // search the cache for a free block
+        free_blocks_type::iterator free_block = free_blocks.find(num_bytes);
+ 
+        if (free_block != free_blocks.end())
+        {
+            //std::cout << "cached_allocator::allocator(): found a hit" << std::endl;
+ 
+            // get the pointer
+            result = free_block->second;
+ 
+            // erase from the free_blocks map
+            free_blocks.erase(free_block);
+        }
+        else
+        {
+            // no allocation of the right size exists
+            // create a new one with cuda::malloc
+            // throw if cuda::malloc can't satisfy the request
+            try
+            {
+                std::cout << "cached_allocator::allocator(): no free block found; calling cuda::malloc" << std::endl;
+ 
+                // allocate memory and convert cuda::pointer to raw pointer
+                result = thrust::cuda::malloc<char>(num_bytes).get();
+            }
+            catch(std::runtime_error &e)
+            {
+                throw;
+            }
+        }
+ 
+        // insert the allocated pointer into the allocated_blocks map
+        allocated_blocks.insert(std::make_pair(result, num_bytes));
+ 
+        return result;
+    }
+ 
+    void deallocate(char* ptr, size_t n)
+    {
+        // erase the allocated block from the allocated blocks map
+        allocated_blocks_type::iterator iter = allocated_blocks.find(ptr);
+        std::ptrdiff_t num_bytes = iter->second;
+        allocated_blocks.erase(iter);
+ 
+        // insert the block into the free blocks map
+        free_blocks.insert(std::make_pair(num_bytes, ptr));
+    }
+ 
+private:
+    typedef std::multimap<std::ptrdiff_t, char*> free_blocks_type;
+    typedef std::map<char*, std::ptrdiff_t> allocated_blocks_type;
+ 
+    free_blocks_type free_blocks;
+    allocated_blocks_type allocated_blocks;
+ 
+    void free_all()
+    {
+        std::cout << "cached_allocator::free_all(): cleaning up after ourselves..." << std::endl;
+ 
+        // deallocate all outstanding blocks in both lists
+        for (free_blocks_type::iterator i = free_blocks.begin();
+                i != free_blocks.end(); i++)
+        {
+            // transform the pointer to cuda::pointer before calling cuda::free
+            thrust::cuda::free(thrust::cuda::pointer<char>(i->second));
+        }
+ 
+        for (allocated_blocks_type::iterator i = allocated_blocks.begin();
+                i != allocated_blocks.end(); i++)
+        {
+            // transform the pointer to cuda::pointer before calling cuda::free
+            thrust::cuda::free(thrust::cuda::pointer<char>(i->first));
+        }
+    }
+};
 
 class CudaTimer
 {
@@ -512,8 +614,10 @@ aabbi CudaSVT<T>::boundary(aabbi bbox, const cudaStream_t& stream) const
   aabbi init;
   init.invalidate();
 
+  static cached_allocator alloc;
   return thrust::reduce(
-      thrust::cuda::par.on(stream),
+      thrust::cuda::par(alloc),
+      //thrust::device,
       boxes_.begin() + min_index,
       boxes_.begin() + max_index + 1,
       init,
@@ -614,7 +718,7 @@ void CudaKdTree::Impl::node_splitting(NodePtr& n)
   if (len[axis] <= 8)
     return;
 
-  static const int NumBins = 8;
+  static const int NumBins = 16;
 
   int dl = len[axis] / NumBins;
 
